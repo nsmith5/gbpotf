6e8f58cb2c9a08ab6701d64acd7675082dfa7c86
Nathan Smith <nathan@felixandpaul.com>
2019-04-13 01:13:27 -0700

    Commit build in /docs folder for github pages

---
+++ docs/precache-manifest.dd9e313ff01c091554cf136e4b42b0e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c20d44c630fb703eba77fbf072dcf8c9",
+    "url": "/static/media/letter.c20d44c6.png"
+  },
+  {
+    "revision": "b93b12ee6c4f948432a3d1c6e7f6034a",
+    "url": "/static/media/DomaineSans_1P25.b93b12ee.otf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "332417b1485abe4ca390",
+    "url": "/static/js/main.b0740843.chunk.js"
+  },
+  {
+    "revision": "77257bb15b62b6c5a055",
+    "url": "/static/js/2.156c4218.chunk.js"
+  },
+  {
+    "revision": "332417b1485abe4ca390",
+    "url": "/static/css/main.bd54704f.chunk.css"
+  },
+  {
+    "revision": "77257bb15b62b6c5a055",
+    "url": "/static/css/2.b884d23b.chunk.css"
+  },
+  {
+    "revision": "e957f11db757c1ac9ce2aced25cf572c",
+    "url": "/index.html"
+  }
+];
