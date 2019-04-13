e6de4adebeedaa1d7a2412f638658734371d544b
Nathan Smith <nathan@felixandpaul.com>
2019-04-13 01:23:59 -0700

    Updates

---
+++ precache-manifest.66b6cfdec8e6d1dd70cf4901eefd4315.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c20d44c630fb703eba77fbf072dcf8c9",
+    "url": "/gbpotf/static/media/letter.c20d44c6.png"
+  },
+  {
+    "revision": "b93b12ee6c4f948432a3d1c6e7f6034a",
+    "url": "/gbpotf/static/media/DomaineSans_1P25.b93b12ee.otf"
+  },
+  {
+    "revision": "95047f88fbec36e8d093",
+    "url": "/gbpotf/static/js/runtime~main.71061e94.js"
+  },
+  {
+    "revision": "c1c52d4c04cbd64f55b6",
+    "url": "/gbpotf/static/js/main.8dadd398.chunk.js"
+  },
+  {
+    "revision": "77257bb15b62b6c5a055",
+    "url": "/gbpotf/static/js/2.156c4218.chunk.js"
+  },
+  {
+    "revision": "c1c52d4c04cbd64f55b6",
+    "url": "/gbpotf/static/css/main.f9ebd566.chunk.css"
+  },
+  {
+    "revision": "77257bb15b62b6c5a055",
+    "url": "/gbpotf/static/css/2.b884d23b.chunk.css"
+  },
+  {
+    "revision": "eeb7083b999aae3d07724a6f7da47cfc",
+    "url": "/gbpotf/index.html"
+  }
+];
