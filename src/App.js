d2a9098d7c265e96a13dd750643c9226aa3fa39e
Nathan Smith <nathan@felixandpaul.com>
2019-04-14 15:57:22 -0700

    Set up ticker and pin font to the price today

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./Home.js"
 import Feed from "./Feed.js"
 import Download from "./Download.js"
 
+
 function NavItem(props) {
 	return (
 		<li className="nav-item">
@@ -22,8 +23,8 @@ function Nav() {
 			<Link className="nav-item" to="/">GBP.OTF</Link>
 			<ul className="nav justify-content-right" display="inline">
 				<NavItem url="/feed" text="FEED" />
-				<NavItem url="/about" text="ABOUT" />	
-				<NavItem url="/download" text="DOWNLOAD" />	
+				<NavItem url="/about" text="ABOUT" />
+				<NavItem url="/download" text="DOWNLOAD" />
 			</ul>
 		</div>
 		</nav>
@@ -31,19 +32,85 @@ function Nav() {
 }
 
 class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			date: new Date(),
+			price: 1.40,
+			history: [
+				{ date: new Date(2018, 3, 1), price: 1.30 },
+				{ date: new Date(2018, 3, 2), price: 1.31 },
+				{ date: new Date(2018, 3, 3), price: 1.31 },
+			]
+		}
+	}
+
+	componentWillMount() {
+		var date = new Date()
+		var params = {
+			startAt: '2010-01-01',
+			endAt: `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}`,
+			symbols: 'EUR',
+			base: 'GBP',
+		}
+		
+		var url = 'https://api.exchangeratesapi.io/history' +
+			`?base=${params.base}&symbols=${params.symbols}&start_at=${params.startAt}&` +
+			`end_at=${params.endAt}`
+		
+		fetch(url, { mode: 'cors'}).then(resp => {
+			return resp.json()
+		}).then(json => {
+			var newHistory = Object.entries(json.rates).map(([date, price]) => {
+				return { date: new Date(date), price: price.EUR }		
+			})
+			newHistory.sort((a, b) => a.date < b.date)
+			var { date, price } = newHistory[0]
+
+			this.setState({ 
+				history: newHistory,
+				date: date,
+				price: price,
+			})
+		})
+	}
+
 	render() {
+		// Probably a bad side effect behaviour, but oh well..
+		if (this.state.price < 1.10) {
+			document.body.style.fontFamily = "\"GBP OTF 1.05\""	
+		} else if (this.state.price < 1.15) {
+			document.body.style.fontFamily = "\"GBP OTF 1.10\""
+		} else if (this.state.price < 1.20) {
+			document.body.style.fontFamily = "\"GBP OTF 1.15\""
+		} else if (this.state.price < 1.25) {
+			document.body.style.fontFamily = "\"GBP OTF 1.20\"" 
+		} else if (this.state.price < 1.30) {
+			document.body.style.fontFamily = "\"GBP OTF 1.25\"" 
+		} else if (this.state.price < 1.35) {
+			document.body.style.fontFamily = "\"GBP OTF 1.30\"" 
+		} else if (this.state.price < 1.40) {
+			document.body.style.fontFamily = "\"GBP OTF 1.35\"" 
+		} else {
+			document.body.style.fontFamily = "\"GBP OTF 1.40\""
+		}
+
 		return (
 			<Router basename={process.env.PUBLIC_URL}>
-				<Nav/>
+				<Nav />
 				<div className="container mt-4">
-					<Route exact path="/" component={Home} />
+					<Route
+						exact
+						path="/"
+						render={() => <Home data={this.state.history} /> }
+					/>
 					<Route path="/download" component={Download} />
 					<Route path="/feed" component={Feed} />
 					<Route path="/about" component={About} />
 				</div>
 			</Router>
 		)
-  	}
+	}
 }
 
-export default App;
+export default App
--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,102 @@
 import React, { Component } from 'react'
-import * as d3 from "d3"
+import { extent } from 'd3-array'
+import { scaleLinear, scaleTime } from 'd3-scale'
+import { line } from 'd3-shape'
+import { select } from 'd3-selection'
+import { axisBottom, axisLeft } from 'd3-axis'
 
-class SparkLine extends Component {
+import './Home.css'
+
+class SparkLine2 extends Component {
 	constructor(props) {
 		super(props)
+		this.margin = {top: 10, right: 30, bottom: 30, left: 60}
+    	this.width = props.width - this.margin.left - this.margin.right
+    	this.height = props.height - this.margin.top - this.margin.bottom
+	}
+	
+	componentDidMount() {
+		this.svg = select(this._rootNode)
+			.append("svg")
+				.attr("width", this.width + this.margin.left + this.margin.right)
+				.attr("height", this.height + this.margin.top + this.margin.bottom)
+			  .append("g")
+				.attr("transform",
+					"translate(" + this.margin.left + "," + this.margin.top + ")");
 
-		var width = 100
-		var height = 25
-		this.x = d3.scaleLinear().range([0, width])
-		this.y = d3.scaleLinear().range([height, 0])
-		this.parseDate = d3.timeFormat("%Y-%b-%d").parse
-		//this.line = d3.svg.line()
-		//		.x(d => this.x(d.date))
-		//	.y(d => this.x(d.value))
+
+		// Add X axis --> it is a date format
+		var x = scaleTime()
+			.domain(extent(this.props.data, d => d.date ))
+		 	.range([ 0, this.width ]);
+		this.svg.append("g")
+			.attr("class", "bottom-axis")
+		 	.attr("transform", "translate(0," + this.height + ")")
+		 	.call(axisBottom(x));
+
+		// Add Y axis
+		var y = scaleLinear()
+			.domain(extent(this.props.data, d => d.price))
+		  .range([this.height, 0]);
+		this.svg.append("g")
+			.attr("class", "left-axis")
+		  	.call(axisLeft(y));
+
+		// Add the line
+		this.svg.append("path")
+			.datum(this.props.data)
+			.attr("class", "line")
+			.attr("fill", "none")
+		  	.attr("stroke", "white")
+		  	.attr("stroke-width", 1)
+		  	.attr("d", line()
+				.x( d =>  x(d.date) )
+				.y( d => y(d.price) )
+			)
 	}
 
-	componentDidMount() {
-		fetch(
-			'https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2019-04-01&symbols=EUR&base=GBP')
-			.then(resp => {
-				console.log(resp.json())
-			})
-		
+	componentDidUpdate() {
+		// Add X axis --> it is a date format
+		var x = scaleTime()
+			.domain(extent(this.props.data, d => d.date ))
+		 	.range([ 0, this.width ]);
+		this.svg.select(".bottom-axis")
+		 	.attr("transform", "translate(0," + this.height + ")")
+		 	.call(axisBottom(x));
+
+		// Add Y axis
+		var y = scaleLinear()
+			.domain(extent(this.props.data, d => d.price))
+		  .range([this.height, 0]);
+		this.svg.select(".left-axis")
+		  .call(axisLeft(y));
+
+		// Add the line
+		this.svg.select(".line")
+			.datum(this.props.data)
+			.attr("fill", "none")
+		  	.attr("stroke", "white")
+		  	.attr("stroke-width", 1)
+		  	.attr("d", line()
+				.x( d =>  x(d.date) )
+				.y( d => y(d.price) )
+			)
+	}
+
+
+	_setRef(componentNode) {
+		this._rootNode = componentNode
 	}
 
 	render() {
-		var styles = {
-			fill: "none",
-			stroke: "#ffff",
-			strokeWidth: 0.5,
-		}
-		return <svg style={styles} id="ticker"></svg>
+		return <div style={{textAlign: 'center'}} className="line-container" ref={this._setRef.bind(this)} />
 	}
 }
 
-function Home() {	
+
+function Home(props) {	
 	return (
-		<SparkLine />
+		<SparkLine2 height={300} width={700} data={props.data}/>
 	)
 }
 
